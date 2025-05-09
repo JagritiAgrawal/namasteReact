954ebff84dd0ecbb4a6489fb2ad8e03ac6bc5825
Jagriti Agrawal <jagritiagr03@gmail.com>
2025-05-09 23:35:13 +0530

    Component Composition in REACT

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const elem = <span> React Element </span>
+
+const title  = (
+  <h1 className = 'head'  tabIndex = "5" >
+    {elem}
+    Namaste React using JSX .
+  </h1>
+)
+
+// JSX ==HTML like or XML like syntax
+const Title = () => {
+  return (<h1 className='head' tabIndex = '1'> Namaste React from JSX Functional Component</h1> ) ;
+} 
+
+const HeadingComponent = () => {
+
+return (<div id='container'>
+  {title}
+  <Title />
+  <h1 id ='heading'> Namaste React Functional Component </h1>
+  </div>);
+}
+
+const root = ReactDOM.createRoot (document.getElementById('root'));
+
+root.render (<HeadingComponent />);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // <div id ="root">
 //<h1>
 // Hello World From React !
@@ -5,24 +49,24 @@
 // </div>
 
 // Below code will be used for injecting the heading tag inside div which has id 'root' using REACT library.
-const  reactHeading = React.createElement('h1', {}, "Hello World from React !");
+//const  reactHeading = React.createElement('h1', {}, "Hello World from React !");
 
-console.log(reactHeading); //It is an object
+//console.log(reactHeading); //It is an object
           
-const reactRoot = ReactDOM.createRoot(document.getElementById('root'));
+//const reactRoot = ReactDOM.createRoot(document.getElementById('root'));
 
-reactRoot.render(reactHeading);  // render method is taking the object and converting it into HTML tag and attaching that HTML inside div named as root
+//reactRoot.render(reactHeading);  // render method is taking the object and converting it into HTML tag and attaching that HTML inside div named as root
 
 
 // <div id ="root">
 //<h1 id= 'heading' xyz=''abc> Hello World from React !</h1>
 // </div>
 
-const  reactHeadingWithAttribute = React.createElement('h1', {id: 'heading', xyz: 'abc' }, "Hello World from React !");
+//const  reactHeadingWithAttribute = React.createElement('h1', {id: 'heading', xyz: 'abc' }, "Hello World from React !");
           
-const reactRootForHeadingAttribute = ReactDOM.createRoot(document.getElementById('root'));
+//const reactRootForHeadingAttribute = ReactDOM.createRoot(document.getElementById('root'));
 
-reactRootForHeadingAttribute.render(reactHeadingWithAttribute);
+//reactRootForHeadingAttribute.render(reactHeadingWithAttribute);
 
 
 
@@ -91,12 +135,12 @@ reactRootForHeadingAttribute.render(reactHeadingWithAttribute);
 //                         *******************************************************************************//
 
 //Example 4
-                                  const parent = React.createElement("div", {id: 'parent'}, [React.createElement("div", {id: "child"},[React.createElement('h1', {}, 'I am an H1 tag'),React.createElement('h2', {}, 'I am an H2 tag')] ),React.createElement("div", {id: "child2"},[React.createElement('h1', {}, 'I am an child2H1 tag'),React.createElement('h2', {}, 'I am an child2H2 tag')] )] )
+                                 // const parent = React.createElement("div", {id: 'parent'}, [React.createElement("div", {id: "child"},[React.createElement('h1', {}, 'I am an H1 tag'),React.createElement('h2', {}, 'I am an H2 tag')] ),React.createElement("div", {id: "child2"},[React.createElement('h1', {}, 'I am an child2H1 tag'),React.createElement('h2', {}, 'I am an child2H2 tag')] )] )
 
   
-    console.log(parent);// So basically heading is an object which is ultimately an React Element
+   // console.log(parent);// So basically heading is an object which is ultimately an React Element
         
-         const root = ReactDOM.createRoot(document.getElementById("root"));
+      //   const root = ReactDOM.createRoot(document.getElementById("root"));
          
-          root.render(parent); // Render function will convert this object into  h1 tag and then put on The DOM under root div present 
+       //   root.render(parent); // Render function will convert this object into  h1 tag and then put on The DOM under root div present 
                                   //in the DOM.
